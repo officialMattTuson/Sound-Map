1bb9f9c8a3848c56e6677dd1df26281eddfb5614
Matthew Tuson <mtuson1414@gmail.com>
2025-02-01 15:07:06 +1300

    remove ssr as will not be needed for this project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { SoundGridComponent } from "../components/sound-grid/sound-grid.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SoundGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
